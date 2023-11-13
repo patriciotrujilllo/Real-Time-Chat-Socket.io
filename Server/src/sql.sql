279e98353cae4386b5bbab0406773c6ccaf0f6a1
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-13 14:08:56 -0300

    sql

---
+++ Server/src/sql.sql
@@ -0,0 +1,47 @@
+-- Active: 1686084594206@@127.0.0.1@3306@chat
+
+#Se crea la base de datos
+
+CREATE DATABASE IF NOT EXISTS chat;
+
+#Se crea tabla de messages
+
+CREATE TABLE
+    messages(
+        id VARCHAR(36) not NULL,
+        content VARCHAR(255) NOT NULL,
+        date TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+        emailUser VARCHAR(36) not NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (emailUser) REFERENCES users(email)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+#Estructura de para la tabla de roles
+
+CREATE TABLE
+    roles (
+        id int NOT NULL,
+        name VARCHAR(10),
+        PRIMARY KEY(id)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+#Estructura de tabla para user
+
+CREATE TABLE
+    users (
+        id VARCHAR(36) not NULL,
+        firstName varchar(45) NOT NULL,
+        lastName varchar(45) DEFAULT NULL,
+        active tinyint(1) NOT NULL DEFAULT 1,
+        email varchar(150) NOT NULL,
+        password varchar(150) NOT NULL,
+        img varchar(150) NOT NULL,
+        roleId int NOT NULL,
+        PRIMARY KEY (email),
+        FOREIGN KEY (roleId) REFERENCES roles(id)
+    )
+
+INSERT INTO roles(id, name)
+VALUES(1, 'admin')
+INSERT INTO roles(id, name)
+VALUES(2, 'user')
