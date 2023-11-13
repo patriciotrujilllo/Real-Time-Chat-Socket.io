0238fb5dc4d4e5aaddd098bdf1cde5b4a25591de
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-13 16:27:22 -0300

    user validator

---
+++ Server/src/validator/user.validator.js
@@ -0,0 +1,53 @@
+import z from 'zod'
+
+const MAX_FILE_SIZE = 1024 * 1024 * 5;
+const ACCEPTED_IMAGE_MIME_TYPES = [
+    "image/jpeg",
+    "image/jpg",
+    "image/png",
+    "image/webp",
+];
+
+const userShema = z.object({
+
+    firstName: z.string({
+        required_error: 'firstName is required',
+        invalid_type_error: 'name must be a string'
+    }),
+
+    lastName: z.string({
+        required_error: 'lastName is required',
+        invalid_type_error: 'lastName must be a string'
+    }),
+
+    active: z.boolean().never().default(1),
+
+    email: z.email({
+        required_error: 'email is required',
+        invalid_type_error: 'email must be a type @email'
+    }),
+
+    img: z.any().refine((files) => {
+            return files?.[0]?.size <= MAX_FILE_SIZE;
+            }, `Max image size is 5MB.`)
+            .refine(
+            (files) => ACCEPTED_IMAGE_MIME_TYPES.includes(files?.[0]?.type),
+            "Only .jpg, .jpeg, .png and .webp formats are supported."
+        ),
+    password: z.string({
+        required_error: 'password is required'
+    }),
+    confirmpassword: z.string({
+        required_error: 'password is required'
+    })
+    .refine((data)=>data.password === data.confirmpassword, {
+        message: 'password do not match',
+        path: ['newPassword']
+    }),
+    roleId: z.number().default(2)
+
+    })
+
+export const validate = (object) =>{
+    return userShema.safeParse(object)
+}
