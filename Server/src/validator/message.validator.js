469bd42b0f00761c411721fafd9a9564a5e64c99
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-13 16:26:59 -0300

    message validator

---
+++ Server/src/validator/message.validator.js
@@ -0,0 +1,16 @@
+import z from 'zod'
+
+const messagesShema = z.object({
+    content: z.string({
+        required_error: 'message is required',
+        invalid_type_error: 'message must be a string'
+    }),
+    emailUser: z.string().email({
+        required_error: 'email is required',
+        invalid_type_error: 'email must be a type email'
+    })
+})
+
+export const validate = (object) =>{
+    return messagesShema.safeParse(object)
+}
