dcb2fecd9a8010bd40b3c896b55f9a19b32707bf
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-14 20:51:43 -0300

    move logic to controller user

--- Server/src/routes/user.router.js
+++ Server/src/routes/user.router.js
@@ -1,54 +1,9 @@
 import { Router } from "express";
-import { validateUser} from '../validator/user.validator.js'
-import jsonwebtoken from 'jsonwebtoken'
-import crypto from 'crypto'
 import multiparty from 'connect-multiparty'
-import bcrypt from 'bcrypt'
-import { addUser } from "../models/user.model.js";
+import { addUserController } from "../controllers/user.controller.js";
 
 
 const multipartyMiddleware = multiparty({uploadDir: 'src/uploads/users'})
 export const userRouter = Router()
 
-userRouter.post('/',multipartyMiddleware,async(req,res)=>{
-
-    const newvalidate = {...req.body,img:req.files.img}
-    const validate = validateUser(newvalidate)
-    
-    if(validate.error){
-        return res.status(400).json({ error: JSON.parse(validate.error.message)})
-    }
-
-    const {email,password} = validate.data
-
-    const file = req.files.img
-
-    const ulrImage = (file) =>{
-        const arrayPath = file.path.split('\\')
-        const nameImg = arrayPath.pop()
-        const folderImg = arrayPath.pop()
-        const pathToSave = `${folderImg}/${nameImg}`
-        return pathToSave
-    }
-    const imgPath = ulrImage(file)
-
-    const forToken = {
-        email
-    }
-    
-
-    const token = jsonwebtoken.sign(forToken,process.env.SIGN_TOKEN)
-    const id = crypto.randomUUID()
-
-
-    try {
-        const hashed_password = await bcrypt.hash(password,10)
-
-        await addUser({...validate.data,password:hashed_password,id,img:imgPath})
-
-        res.status(200).json({email,token})
-
-    } catch (error) {
-        return res.status(400).json({message:'error at insert',error})
-    }
-})
+userRouter.post('/',multipartyMiddleware,addUserController)
