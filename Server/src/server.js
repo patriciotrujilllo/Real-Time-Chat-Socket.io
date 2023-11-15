750584029059773c7fd2bd3bb76f0b9695d6f2f6
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-15 11:57:05 -0300

    add router messages, and express.json()

--- Server/src/server.js
+++ Server/src/server.js
@@ -2,13 +2,16 @@ import express from 'express'
 import logger from 'morgan'
 import {config} from 'dotenv'
 import { userRouter } from './routes/user.router.js'
+import { messagesRouter } from './routes/messages.router.js'
 
 config()
 
 const app = express()
+app.use(express.json())
 app.use(logger('dev'))
 
 app.use('/user',userRouter)
+app.use('/message',messagesRouter)
 
 const PORT = process.env.PORT || 3000
 
