4d355179229ca988c1379d76d3e228e8f6721440
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-15 15:41:07 -0300

    change function for class

--- Server/src/models/message.model.js
+++ Server/src/models/message.model.js
@@ -1,17 +1,21 @@
 import { pool } from './DBConnection.js'
 
-export const addMessage = async({id,content,date,emailUser}) =>{
+export class MessageModel {
+
+    async addMessage ({id,content,date,emailUser}){
     
-    const result = await pool.query('INSERT INTO messages(id,content,date,emailUser) VALUES(?,?,?,?)',[id,content,date,emailUser])
+        const result = await pool.query('INSERT INTO messages(id,content,date,emailUser) VALUES(?,?,?,?)',[id,content,date,emailUser])
+    
+        return result
+    }
 
-    return result
+    async showMessagesUser ({email}) {
+        const result = await pool.query('SELECT * FROM messages WHERE emailUser=?',[email])
+        return result
+    }
 
-}
-export const showMessagesUser = async({user}) =>{
-    const result = await pool.query('SELECT * FROM messages WHERE emailUser=?',[user])
-    return result
-}
-export const showMessagesAll = async() =>{
-    const result = await pool.query('SELECT * FROM messages')
-    return result
+    async showMessagesAll () {
+        const result = await pool.query('SELECT * FROM messages')
+        return result
+    }
 }
