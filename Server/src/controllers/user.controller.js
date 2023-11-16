b1c07ed1698301926fba66fe0825f38ada29ff01
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-16 20:56:22 -0300

    change name of model , remove jwt, add login of user

--- Server/src/controllers/user.controller.js
+++ Server/src/controllers/user.controller.js
@@ -1,11 +1,11 @@
 import { validateUser} from '../validator/user.validator.js'
-import jsonwebtoken from 'jsonwebtoken'
 import crypto from 'crypto'
 import { UserModel } from "../models/user.model.js";
-import { hashPassword } from '../utils/auth.js';
+import { compareHashPassword, hashPassword } from '../utils/auth.js';
 import { deletelinkFile, ulrImage } from '../utils/pathOfImg.js';
+import { createAccessToken, createRefreshToken } from '../utils/jwt.js';
 
-const Model = new UserModel
+const User = new UserModel
 
 export const addUserController = async(req,res)=>{
 
@@ -16,28 +16,48 @@ export const addUserController = async(req,res)=>{
         return res.status(400).json({ error: JSON.parse(validate.error.message)})
     }
 
-    const {email,password} = validate.data
+    const { password } = validate.data
     const file = req.files.img
     const imgPath = ulrImage(file)
-
-    const forToken = {
-        email
-    }
     
-
-    const token = jsonwebtoken.sign(forToken,process.env.SIGN_TOKEN)
     const id = crypto.randomUUID()
-
 
     try {
         const hashed_password = await hashPassword({password})
 
-        await Model.add({...validate.data,password:hashed_password,id,img:imgPath})
-
-        res.status(200).json({email,token})
+        await User.add({...validate.data,password:hashed_password,id,img:imgPath})
+        
+        res.status(200).json({ response:'user add'})
 
     } catch (error) {
         deletelinkFile({path:imgPath})
-        return res.status(400).json({message:'error at insert',error})
+        return res.status(400).json({message:'error at insert user',error})
+    }
+}
+
+export const login = async(req,res) =>{
+    const {email,password} = req.body
+
+    try {
+        const result = await User.getByEmail({email})
+        
+        if (!(result[0][0].active)) return res.status(400).json({message:'user is not active',error})
+
+        await compareHashPassword({password,hashed_password:result[0][0].password})
+
+        const userReturn = {
+            id: result[0][0].id,
+            firstName:result[0][0].firstName ,
+            lastName:result[0][0].lastName,
+            email: result[0][0].email
+        }
+        
+        res.status(200).json({
+            token:createAccessToken(userReturn),
+            refresh:createRefreshToken(userReturn)
+    })
+
+    } catch (error) {
+        return res.status(401).json({error: 'Invalid user o password'})
     }
 }
