de7592b862aa53705afd9269b906bbd309908819
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-16 14:25:18 -0300

    add urlImg and delete file in case of error

---
+++ Server/src/utils/pathOfImg.js
@@ -0,0 +1,18 @@
+import fs from 'fs'
+
+export const ulrImage = (file) =>{
+    const arrayPath = file.path.split('\\')
+    const nameImg = arrayPath.pop()
+    const folderImg = arrayPath.pop()
+    const pathToSave = `${folderImg}/${nameImg}`
+    return pathToSave
+}
+export const deletelinkFile = ({path}) =>{
+    try {
+        if(!path) throw new Error('there is not image to delete')
+        console.log(path)
+        fs.unlinkSync(`src/uploads/${path}`)
+    } catch (error) {
+        console.error(error)
+    }
+}
