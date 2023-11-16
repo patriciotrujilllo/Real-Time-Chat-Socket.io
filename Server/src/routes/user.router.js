0d2619b6fb999e7f94a29ef6083d7371707b5059
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-16 20:55:28 -0300

    add login

--- Server/src/routes/user.router.js
+++ Server/src/routes/user.router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import multiparty from 'connect-multiparty'
-import { addUserController } from "../controllers/user.controller.js";
+import { addUserController,login } from "../controllers/user.controller.js";
 
 
 const multipartyMiddleware = multiparty({uploadDir: 'src/uploads/users'})
 export const userRouter = Router()
 
 userRouter.post('/',multipartyMiddleware,addUserController)
+userRouter.post('/login', login)
