3e38bd880f607b2a62cad3777fc8c7a8f235b7fc
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-16 20:52:48 -0300

    add function to compare password with hashed password

--- Server/src/utils/auth.js
+++ Server/src/utils/auth.js
@@ -4,3 +4,7 @@ export const hashPassword = async({password}) =>{
     const hashed_password = await bcrypt.hash(password,10)
     return hashed_password
 }
+export const compareHashPassword = async({password,hashed_password}) => {
+    const result = await bcrypt.compare(password,hashed_password)
+    return result
+}
