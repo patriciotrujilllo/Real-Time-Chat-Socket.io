3cecc29e9eb6dd73f618666b179d22cba65a31df
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-16 14:23:03 -0300

    change function to class

--- Server/src/models/user.model.js
+++ Server/src/models/user.model.js
@@ -1,7 +1,29 @@
 import { pool } from './DBConnection.js'
 
-export const addUser = async({id,firstName,lastName,active,email,password,roleId,img}) =>{
-    
-        await pool.query('INSERT INTO users(id, firstName, lastName, active, email, password, img, roleId) VALUES(?,?,?,?,?,?,?,?)',[id,firstName,lastName,Number(active),email,password,img,roleId])
+export class UserModel {
+        async add ({id,firstName,lastName,active,email,password,roleId,img}) {
 
+                await pool.query('INSERT INTO users(id, firstName, lastName, active, email, password, img, roleId) VALUES(?,?,?,?,?,?,?,?)',[id,firstName,lastName,Number(active),email,password,img,roleId])
+        
+        }
+        async getByEmail ({email}) {
+                const result = await pool.query('SELECT * FROM users WHERE emailUser=?',[email])
+                return result
+        }    
+        async getById ({id}) {
+                const result = await pool.query('SELECT * FROM users WHERE id=?',[id])
+                return result
+        }   
+        async getAll () {
+                const result = await pool.query('SELECT * FROM users')
+                return result
+        }
+        async update () {
+                
+        }
+        async delete ({email}) {
+        const result = await pool.query('DELETE * FROM users WHERE emalUser=?',[email])
+
+        return result
+        }
 }
