b58ebd51c7fa3cb1733abe5b01957a3ab910fe1d
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-16 09:55:49 -0300

    se crea servido http y de socket io

--- Server/src/server.js
+++ Server/src/server.js
@@ -3,16 +3,34 @@ import logger from 'morgan'
 import {config} from 'dotenv'
 import { userRouter } from './routes/user.router.js'
 import { messagesRouter } from './routes/messages.router.js'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+import { corsConfiguration } from './utils/corsConfiguration.js'
 
 config()
 
 const app = express()
 app.use(express.json())
+app.use(express.static('/Server/src/uploads'))
 app.use(logger('dev'))
+app.use(cors(corsConfiguration()))
+
+const server = createServer(app)
+const io = new Server(server,{
+    cors: corsConfiguration
+})
+
+io.on('connection',(socket)=>{
+    console.log('user connected')
+
+    socket.on('disconnect', () =>{
+        console.log('user disconnected')
+    })
+}) 
 
 app.use('/user',userRouter)
 app.use('/message',messagesRouter)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT,()=>{console.log(`server running on port: ${PORT}`)})
+server.listen(PORT,()=>{console.log(`server running on port: ${PORT}`)})
