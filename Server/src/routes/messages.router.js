ce4e8f2ea6adcc979cd13222259284b84be92bca
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-17 11:42:12 -0300

    add new method for messages

--- Server/src/routes/messages.router.js
+++ Server/src/routes/messages.router.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { addUserMessage,getMessages } from "../controllers/messages.controller.js";
+import { addUserMessage,getMessages,getMessagesById,deleteMessage } from "../controllers/messages.controller.js";
+import { userAuthenticated } from "../middleware/autorization.js";
 
 
 
 export const messagesRouter = Router()
 
-messagesRouter.post('/',addUserMessage)
-messagesRouter.get('/',getMessages)
+messagesRouter.post('/',userAuthenticated,addUserMessage)
+messagesRouter.get('/:id',userAuthenticated,getMessagesById)
+messagesRouter.get('/',userAuthenticated,getMessages)
+messagesRouter.delete('/:id',userAuthenticated,deleteMessage)
