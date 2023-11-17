5e1e00718e73f7092d22a766be1fcbf430928fce
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-17 19:12:53 -0300

    update app

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css'
+import { io } from 'socket.io-client'
+import { useState,useEffect } from 'react'
+
+const socket = io('http://localhost:4000')
 
 function App() {
+
+  const [isConnected,setIsConnected] = useState(false)
+  // const [newMessage,setNewMessage] = useState('')
+  // const [messages,setMessages] = useState([])
+
+  useEffect(()=>{
+
+    socket.on('connect',()=> setIsConnected(true))
+
+  },[])
 
   return (
     <>
       <h1>Esta Funcionando</h1>
+      <h2>{isConnected ? 'Esta conectado al socket': 'No se conecto al socket'}</h2>
+      
     </>
   )
 }
