929c199ebe06ef2af727a4e988b5406ee02c7417
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-21 19:29:37 -0300

    inprove loin

--- App/src/components/Login.jsx
+++ App/src/components/Login.jsx
@@ -1,21 +1,25 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import { login } from "../services/user"
 import { useCookies} from 'react-cookie'
-
+// import { Navigate } from "react-router-dom"
 
 export const Login = () => {
 
     const [changeEmail,setChangeEmail] = useState('')
     const [changePassword,setChangePassword] = useState('')
     const [error,setError] = useState('')
-    const [,setCookie] = useCookies('token')
+    const [cookies,setCookie] = useCookies(['token'])
+
+    useEffect(()=>{
+        console.log(cookies)
+    },[cookies])
     
     const handleForm = async(e) => {
         e.preventDefault()
         try {
             const data = await login({email:changeEmail,password:changePassword})
             console.log(data)
-            setCookie('token',data,{ path: '/App'})
+            setCookie('token',data,{ path: '/App' })
         } catch (err) {
             console.error(err)
             setError(err)
@@ -26,12 +30,25 @@ export const Login = () => {
     return (
         <div className="container">
             <form onSubmit={handleForm}>
-                <label htmlFor="">
-                    Email: <input type="text" onChange={(e)=>setChangeEmail(e.target.value)}/>
-                </label>
-                <label htmlFor="">
-                    password: <input type="text" onChange={(e)=>setChangePassword(e.target.value)}/>
-                </label>
+                <label htmlFor="username">Username</label>
+                <input 
+                type="text" 
+                id="username"
+                autoComplete="off"
+                onChange={(e)=>setChangeEmail(e.target.value)}
+                value={changeEmail}
+                required
+                />
+    
+                <label htmlFor="password">Password:</label>
+                <input 
+                type="password" 
+                id="password"
+                onChange={(e)=>setChangePassword(e.target.value)}
+                value={changePassword}
+                required
+                />
+                
                 <button>Iniciar sesion</button>
             </form>
             {error && error}
