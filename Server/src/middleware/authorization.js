3e491d9b18b96024eae23412c992ce2c64ae06b2
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-25 10:46:58 -0300

    remove console.log

--- Server/src/middleware/authorization.js
+++ Server/src/middleware/authorization.js
@@ -2,7 +2,6 @@ import { decodeToken } from "../utils/jwt.js"
 
 export const userAuthenticated = (req,res, next) =>{
     const  authorization  = req.headers.authorization || req.headers.Authorization
-    console.log(authorization)
     if(!authorization?.startsWith('Bearer ')) return res.status(401).json({message: 'Unauthorized'})
     const token = authorization.replace('Bearer ','')
     
