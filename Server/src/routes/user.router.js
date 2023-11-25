8adff677651a26699b412747b9ae95ed8468840c
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-25 10:48:01 -0300

    add route to user, patch method

--- Server/src/routes/user.router.js
+++ Server/src/routes/user.router.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import multiparty from 'connect-multiparty'
-import { addUserController } from "../controllers/user.controller.js";
-import { userAuthenticated } from "../middleware/authorization.js";
+import { addUserController,updateUserController } from "../controllers/user.controller.js";
+// import { userAuthenticated } from "../middleware/authorization.js";
 
 
 const multipartyMiddleware = multiparty({uploadDir: 'src/uploads/users'})
 export const userRouter = Router()
-userRouter.use(userAuthenticated)
+// userRouter.use(userAuthenticated)
 
 userRouter.post('/',multipartyMiddleware,addUserController)
+userRouter.patch('/',multipartyMiddleware,updateUserController)
