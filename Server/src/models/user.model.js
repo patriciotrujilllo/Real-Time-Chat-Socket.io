01d5761705824c293263fdcbf53992763fc79d1f
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-25 10:47:35 -0300

    add update user and delete user

--- Server/src/models/user.model.js
+++ Server/src/models/user.model.js
@@ -6,6 +6,10 @@ export class UserModel {
                 await pool.query('INSERT INTO users(id, firstName, lastName, active, email, password, img, roleId) VALUES(?,?,?,?,?,?,?,?)',[id,firstName,lastName,Number(active),email,password,img,roleId])
         
         }
+        async getAll () {
+                const result = await pool.query('SELECT * FROM users')
+                return result
+        }
         async getByEmail ({email}) {
                 const result = await pool.query('SELECT * FROM users WHERE email=?',[email])
                 return result
@@ -14,16 +18,24 @@ export class UserModel {
                 const result = await pool.query('SELECT * FROM users WHERE id=?',[id])
                 return result
         }   
-        async getAll () {
-                const result = await pool.query('SELECT * FROM users')
-                return result
-        }
-        async delete ({email}) {
+        async deletebyEmail ({email}) {
                 const result = await pool.query('DELETE FROM users WHERE emailUser=?',[email])
 
         return result
         }
-        async update () {
+        async deletebyId ({id}) {
+                const result = await pool.query('DELETE FROM users WHERE id=?',[id])
+
+        return result
+        }
+        async updatebyEmail (email,data) {
                 
+        }
+        async updatebyId ({id,data}) {
+                const columns = Object.keys(data).map(col => `${col} = ?`).join(', ')
+                const values = Object.values(data)
+                const sql = `UPDATE users SET ${columns} WHERE id = ?`
+                values.push(id)
+                return await pool.query(sql, values)
         }
 }
