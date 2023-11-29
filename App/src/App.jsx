d129bac663d20faf2cecf6dca5d9bc25aabaffe3
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-29 17:53:17 -0300

    add routes and styled styled componend for container

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -4,6 +4,8 @@ import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import { io } from 'socket.io-client'
 import { Login } from './components/Login'
 import { Messages } from './components/Messages'
+import { Register } from './components/Register'
+import styled from 'styled-components'
 
 function App () {
 
@@ -16,17 +18,28 @@ useEffect(() => {
 }, []);
 
   return (
+    <Container>
     <Suspense fallback={<div>Loading...</div>}>
       <BrowserRouter>
         <Routes>
-
-          <Route path="/" element={<Login/>}/>
+          <Route path="/" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
           <Route path="/messages" element={<Messages/>}/>
 
         </Routes>
       </BrowserRouter>
     </Suspense>
+    </Container>
   )
 }
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+`
 
 export default App
