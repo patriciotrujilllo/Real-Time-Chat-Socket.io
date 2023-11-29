78296dca596c5fd7622abec38096efa5285e2cb9
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-29 10:24:22 -0300

    add controller for update messages

--- Server/src/controllers/messages.controller.js
+++ Server/src/controllers/messages.controller.js
@@ -1,4 +1,4 @@
-import { validateMessage} from '../validator/message.validator.js'
+import { validateMessage,validateMessageUpdate} from '../validator/message.validator.js'
 import crypto from 'crypto'
 import { MessageModel } from "../models/message.model.js";
 
@@ -45,19 +45,33 @@ export const addUserMessage = async(req,res)=>{
         return res.status(400).json({message:'error at insert',error})
     }
 }
+export const updateMessages = async(req,res)=>{
+
+    const validate = validateMessageUpdate({...req.body})
+
+    if(validate.error){
+        return res.status(400).json({ error: JSON.parse(validate.error.message)})
+    }
+
+    const {content,idUser,id} = validate.data
+
+    const message = await Message.getById({id})
+    if(!message) return res.status(400).json({message: 'Message not found'})
+
+    try {
+        await Message.update({id,content,idUser})
+        res.status(200).json({ message:'update message'})
+    } catch (error) {
+        return res.status(400).json({message:'error at update message',error})
+    }
+}
 export const getMessages = async(req,res)=>{
 
-    // const {email} = req.query
-
-    // if(email){
-    //     try {
-    //         const result = await Message.get({email})
-    //         console.log(result[0])
-    //         return res.status(200).json(result[0])
-    //     } catch (error) {
-    //         return res.status(400).json(error)
-    //     }
-    // }
+    
+    _getMessages(req,res)
+}
+export const getMessagesWithUser = async(req,res)=>{
+yuh
     
     _getMessages(req,res)
 }
@@ -75,12 +89,16 @@ export const getMessagesById = async(req,res)=>{
 }
 export const deleteMessage = async(req,res) => {
     
-    const {id} = req.params
+    const {id} = req.body
+    if(!id) return res.status(400).json({message: 'Message id Required'})
+
+    const message = await Message.getById({id})
+    if(!(message[0].length)) return res.status(400).json({message:'Message not found'})
 
     try {
         await Message.delete({id})
-        _getMessages(req,res)
+        return res.status(200).json({message: 'message deleted'})
     } catch (error) {
-        res.status(400).json(error)
+        res.status(400).json({message: 'error delete message'})
     }
 }   
