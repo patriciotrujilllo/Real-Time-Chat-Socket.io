a6fc13044100dc4c4fadd2f7420eb7c945ca8b4e
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-29 10:19:11 -0300

    in process handle error

---
+++ Server/src/middleware/allErrors.js
@@ -0,0 +1,4 @@
+export const allErrors = (error,req,res,next) => {
+    error.statusCode = error.statusCode || 500
+    res.status(error.statusCode).json({ status: error.statusCode, message: error.message})
+}
