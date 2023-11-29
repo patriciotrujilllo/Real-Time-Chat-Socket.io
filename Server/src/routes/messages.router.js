e845bd8152662474beeb810764bb7fe5e0125f96
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-29 10:21:42 -0300

    change route of delete message and add update

--- Server/src/routes/messages.router.js
+++ Server/src/routes/messages.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addUserMessage,getMessages,getMessagesById,deleteMessage } from "../controllers/messages.controller.js";
+import { addUserMessage,getMessages,getMessagesById,deleteMessage,updateMessages } from "../controllers/messages.controller.js";
 import { userAuthenticated } from "../middleware/authorization.js";
 
 
@@ -10,4 +10,5 @@ messagesRouter.use(userAuthenticated)
 messagesRouter.post('/',addUserMessage)
 messagesRouter.get('/:id',getMessagesById)
 messagesRouter.get('/',getMessages)
-messagesRouter.delete('/:id',deleteMessage)
+messagesRouter.delete('/',deleteMessage)
+messagesRouter.put('/',updateMessages)
