6b024002ce8e509e3de6c844edb953ca45d354ba
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-11-29 10:22:16 -0300

    add validate for update message

--- Server/src/validator/message.validator.js
+++ Server/src/validator/message.validator.js
@@ -14,3 +14,22 @@ const messagesShema = z.object({
 export const validateMessage = (object) =>{
     return messagesShema.safeParse(object)
 }
+
+const messagesShemaUpdate = z.object({
+    content: z.string({
+        required_error: 'message is required',
+        invalid_type_error: 'message must be a string'
+    }),
+    idUser: z.string({
+        required_error: 'ID of user is required',
+        invalid_type_error: 'idUser must be a type STRING'
+    }),
+    id: z.string({
+        required_error: 'ID is required',
+        invalid_type_error: 'id must be a type STRING'
+    })
+})
+
+export const validateMessageUpdate = (object) =>{
+    return messagesShemaUpdate.safeParse(object)
+}
