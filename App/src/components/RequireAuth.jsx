752e7623d40eca65b26ec23894c1fb6d4fe67623
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-01 17:04:27 -0300

    component for redirect user depends of credentials and role

---
+++ App/src/components/RequireAuth.jsx
@@ -0,0 +1,15 @@
+import { Navigate,Outlet, useLocation } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
+export const RequireAuth = ({ allowedRoles }) =>{
+    const {auth} = useAuth()
+    const location = useLocation()
+    console.log(auth.user)
+    return (
+        allowedRoles?.includes(auth?.role)
+        ? <Outlet /> 
+        : auth?.user 
+            ? <Navigate to="/unauthorized" state={{ from: location }} replace />
+            : <Navigate to="/login" state={{ from: location }} replace />
+    )
+}
