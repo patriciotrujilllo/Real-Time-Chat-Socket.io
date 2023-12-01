366787df7aa3af6a7e87841e77efca119aa8cf38
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-01 17:12:05 -0300

    add private routes and roles for routes

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -5,7 +5,10 @@ import { io } from 'socket.io-client'
 import { Login } from './components/Login'
 import { Messages } from './components/Messages'
 import { Register } from './components/Register'
+import { RequireAuth } from './components/RequireAuth'
 import styled from 'styled-components'
+import { Prueba } from './components/prueba'
+import { Unauthorized } from './components/Unauthorized'
 
 function App () {
 
@@ -15,6 +18,7 @@ useEffect(() => {
   socket.on('connect', () => {
     console.log('Conectado al servidor');
   })
+// eslint-disable-next-line react-hooks/exhaustive-deps
 }, []);
 
   return (
@@ -22,9 +26,22 @@ useEffect(() => {
     <Suspense fallback={<div>Loading...</div>}>
       <BrowserRouter>
         <Routes>
+          {/* Rutas no protegidas/Publicas */}
           <Route path="/" element={<Register/>}/>
           <Route path="/login" element={<Login/>}/>
-          <Route path="/messages" element={<Messages/>}/>
+          <Route path="/unauthorized" element={<Unauthorized/>}/>
+
+          {/* Rutas protegidas, en vez de acerlo una a una, se envuelve todo a la vez*/}
+
+          {/* Rutas donde pueden ingresar usuarios y admimintradores */}
+          <Route element={<RequireAuth allowedRoles={[1,2]} />}>
+            <Route path="/messages" element={<Messages/>}/>
+          </Route>
+
+          {/* Rutas donde pueden ingresar admimintradores */}
+          <Route element={<RequireAuth allowedRoles={[1]} />}>
+            <Route path="/prueba" element={<Prueba/>}/>
+          </Route>
 
         </Routes>
       </BrowserRouter>
