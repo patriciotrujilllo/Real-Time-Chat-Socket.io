8c75d34e818483e16842b25be5e486eea966dfb8
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-01 16:58:40 -0300

    add methods of delete, put and patch

--- Server/src/utils/corsConfiguration.js
+++ Server/src/utils/corsConfiguration.js
@@ -2,7 +2,7 @@ export const corsConfiguration = () => {
     const corsOption = {
         origin: 'http://localhost:5173',
         credentials: true,
-        methods: ['GET', 'POST'],
+        methods: ['GET', 'POST' ,'DELETE','PUT','PATCH'],
         preflightContinue: false,
         optionsSuccessStatus: 200
     }
