fda27fc4f2f1147fdaf965a102082723f9925bc1
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-04 13:15:22 -0300

    context provider for auth

--- App/src/context/AuthProvider.jsx
+++ App/src/context/AuthProvider.jsx
@@ -2,13 +2,25 @@ import { createContext,useState } from "react";
 
 export const AuthContext = createContext()
 
-const initialState = JSON.parse(window.localStorage.getItem('auth')) || {}
+const initialState = JSON.parse(window.localStorage.getItem('auth')) ? JSON.parse(window.localStorage.getItem('auth')) : {}
 
 export const AuthProvider = ({children}) =>{
     const [auth,setAuth] = useState(initialState)
 
+    const updateAuth = ({accessToken}) => {
+        console.log(accessToken)
+        const newAuth = {...auth, accessToken}
+        window.localStorage.setItem('auth', JSON.stringify(newAuth))
+        setAuth(newAuth)
+    }
+
+    const updateAuthLogin = (newAuth) => {
+        window.localStorage.setItem('auth', JSON.stringify(newAuth))
+        setAuth(newAuth)
+    }
+
     return(
-        <AuthContext.Provider value={{auth,setAuth}}>
+        <AuthContext.Provider value={{auth,updateAuth,updateAuthLogin}}>
             {children}
         </AuthContext.Provider>
     )
