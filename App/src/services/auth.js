5143fed34458590345dd3c699c86cd9c0385e9fc
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-04 13:46:48 -0300

    create function for instance of refresToken

---
+++ App/src/services/auth.js
@@ -0,0 +1,11 @@
+import { instanceRefresh } from "./axios"
+
+export const refreshToken = async() => {
+try {
+    const result = await instanceRefresh.get(`/auth/refresh`)
+    return result.data
+} catch (error) {
+    console.log(error)
+}
+    
+}
