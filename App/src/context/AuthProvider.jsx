d34bda62e1102573983388302c46b22f4884d50c
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-04 22:39:30 -0300

    inprove code and remove localStorage

--- App/src/context/AuthProvider.jsx
+++ App/src/context/AuthProvider.jsx
@@ -2,20 +2,21 @@ import { createContext,useState } from "react";
 
 export const AuthContext = createContext()
 
-const initialState = JSON.parse(window.localStorage.getItem('auth')) ? JSON.parse(window.localStorage.getItem('auth')) : {}
+// const initialState = JSON.parse(window.localStorage.getItem('auth')) ? JSON.parse(window.localStorage.getItem('auth')) : {}
 
 export const AuthProvider = ({children}) =>{
-    const [auth,setAuth] = useState(initialState)
+    const [auth,setAuth] = useState({})
 
-    const updateAuth = ({accessToken}) => {
-        console.log(accessToken)
-        const newAuth = {...auth, accessToken}
-        window.localStorage.setItem('auth', JSON.stringify(newAuth))
+    const updateAuth = (result) => {
+        const newAuth = {...result}
+        // window.localStorage.setItem('auth', JSON.stringify(newAuth))
+        console.log(`Esto es en el context: `,newAuth)
         setAuth(newAuth)
     }
 
     const updateAuthLogin = (newAuth) => {
-        window.localStorage.setItem('auth', JSON.stringify(newAuth))
+        console.log(newAuth)
+        // window.localStorage.setItem('auth', JSON.stringify(newAuth))
         setAuth(newAuth)
     }
 
