92be8f5c35bdf3e3bc68e95e86470c13f45911fd
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-04 22:31:37 -0300

    create hook for logout

---
+++ App/src/hooks/useLogout.js
@@ -0,0 +1,19 @@
+import { instanceRefresh } from "../services/axios"; //se utiliza esta instancia por que ya tiene withCredentials, no hay que confundirse con el nombre, podria haber utilizado instance pero tendria que agregarle withCredentials.
+import { useAuth } from "./useAuth";
+
+const useLogout = () => {
+    const { updateAuth } = useAuth()
+
+    const logout = async() => {
+        updateAuth({})
+        try {
+            await instanceRefresh.post('/auth/logout')
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    return logout
+}
+
+export default useLogout
