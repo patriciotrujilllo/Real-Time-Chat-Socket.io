2106c72ee3aa8b0fc6c4626bd843181628301014
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-07 20:58:06 -0300

    update auth login

--- App/src/components/Login.jsx
+++ App/src/components/Login.jsx
@@ -7,58 +7,57 @@ import { useAuth } from "../hooks/useAuth"
 
 export const Login = () => {
     
-    const [email,setEmail] = useState('')
-    const [changePassword,setChangePassword] = useState('')
-    const [error,setError] = useState('')
+	const [email,setEmail] = useState('')
+	const [changePassword,setChangePassword] = useState('')
+	const [error,setError] = useState('')
 
-    const navigate = useNavigate()
-    const location = useLocation()
-    const from = location.state?.from?.pathname || "/messages"
-    const {updateAuthLogin} = useAuth()
+	const navigate = useNavigate()
+	const location = useLocation()
+	const from = location.state?.from?.pathname || "/messages"
+	const {updateAuthLogin} = useAuth()
     
-    const handleForm = async(e) => {
-        e.preventDefault()
-        try {
-            const data = await loginWithAxios({email,password:changePassword})
-            const auth = {accessToken: data.accessToken, email:email , roleId: data.roleId}
-            updateAuthLogin(auth)
-            navigate(from, { replace: true})
+	const handleForm = async(e) => {
+		e.preventDefault()
+		try {
+			const data = await loginWithAxios({email,password:changePassword})
+			updateAuthLogin({accessToken: data})
+			navigate(from, { replace: true})
             
-        } catch (err) {
-            console.error(err)
-            setError(err)
-        }
+		} catch (err) {
+			console.error(err)
+			setError(err)
+		}
         
-    }
+	}
 
-    return (
-        <>
-        <FormContainer>
-            <form onSubmit={handleForm}>
-                <label htmlFor="email">Email </label>
-                <input 
-                type="text" 
-                id="email"
-                autoComplete="off"
-                onChange={(e)=>setEmail(e.target.value)}
-                value={email}
-                required
-                />
+	return (
+		<>
+			<FormContainer>
+				<form onSubmit={handleForm}>
+					<label htmlFor="email">Email </label>
+					<input 
+						type="text" 
+						id="email"
+						autoComplete="off"
+						onChange={(e)=>setEmail(e.target.value)}
+						value={email}
+						required
+					/>
     
-                <label htmlFor="password">Password:</label>
-                <input 
-                type="password" 
-                id="password"
-                onChange={(e)=>setChangePassword(e.target.value)}
-                value={changePassword}
-                required
-                />
+					<label htmlFor="password">Password:</label>
+					<input 
+						type="password" 
+						id="password"
+						onChange={(e)=>setChangePassword(e.target.value)}
+						value={changePassword}
+						required
+					/>
                 
-                <button>Iniciar sesion</button>
-                <span> ¿No tienes una cuenta? <Link to="/">Registrame</Link></span>
-            </form>
-            {error && error}
-        </FormContainer>
-        </>
-    )
+					<button>Iniciar sesion</button>
+					<span> ¿No tienes una cuenta? <Link to="/">Registrame</Link></span>
+				</form>
+				{error && error}
+			</FormContainer>
+		</>
+	)
 }
