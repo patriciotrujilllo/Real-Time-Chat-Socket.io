6402f96a54ea40d065b90fc82a215424aade003e
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-07 20:55:39 -0300

    for now disable ReactDeevsTools is commented

--- App/src/main.jsx
+++ App/src/main.jsx
@@ -1,6 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthProvider.jsx'
+// import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+// if (window.process.env.NODE_ENV === 'production') {
+//   disableReactDevTools();
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
