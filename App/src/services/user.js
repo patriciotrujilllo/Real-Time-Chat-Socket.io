5399dccdc46a4fc92e40f8b2899d6e3055a00be5
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-07 21:02:52 -0300

    update eslint

--- App/src/services/user.js
+++ App/src/services/user.js
@@ -3,23 +3,23 @@ import { instancePublic } from "./axios"
 
 export const loginWithAxios = async({email,password}) =>{
 
-    try {
+	try {
         
-        const result = await instancePublic.post(`/auth/login`,{email,password},{
-            withCredentials: true
-        })
+		const result = await instancePublic.post(`/auth/login`,{email,password},{
+			withCredentials: true
+		})
 
-        return result.data
+		return result.data
 
-    } catch (error) {
-        console.error(`Error al conectar al servidor: ${error}`)
-    }
+	} catch (error) {
+		console.error(`Error al conectar al servidor: ${error}`)
+	}
     
 }
 export const register = async(formData) =>{
         
-        const response = await instancePublic.post(`/user/register`,formData)
-        return response.data
+	const response = await instancePublic.post(`/user/register`,formData)
+	return response.data
     
     
 }
