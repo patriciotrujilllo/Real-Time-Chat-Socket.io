fa531a65f31488cd1efb58da9d39655d7b38859b
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-07 21:01:09 -0300

    update eslint

--- App/src/components/Messages.jsx
+++ App/src/components/Messages.jsx
@@ -4,36 +4,36 @@ import { useNavigate,useLocation } from "react-router-dom"
 import useLogout from "../hooks/useLogout"
 
 export const Messages = () => {
-    const logout = useLogout()
-    const axios = useAxiosRefresh()
-    const navigate = useNavigate()
-    const location = useLocation()
-    // const [messages,setMessages] = useState([])
+	const logout = useLogout()
+	const axios = useAxiosRefresh()
+	const navigate = useNavigate()
+	const location = useLocation()
+	// const [messages,setMessages] = useState([])
 
-    useEffect(()=> {
+	useEffect(()=> {
 
-        (async() => {
-            try{
-                const result = await axios.get('/message')
-                console.log(result?.data)
-            }catch(err) {
-                console.error(err)
-                navigate('/login', { state: { from: location}, replace: true})
-            }
+		(async() => {
+			try{
+				const result = await axios.get('/message')
+				console.log(result?.data)
+			}catch(err) {
+				console.error(err)
+				navigate('/login', { state: { from: location}, replace: true})
+			}
             
-            // setMessages(result?.data)
-        })()
+			// setMessages(result?.data)
+		})()
 
-    },[])
+	},[])
 
-    return (
-        <>
-        <h1>Ha iniciado sesion y esta es la pagina de los mensajes</h1>
-        <h2>Mensajes</h2>
-        {/* {
+	return (
+		<>
+			<h1>Ha iniciado sesion y esta es la pagina de los mensajes</h1>
+			<h2>Mensajes</h2>
+			{/* {
             messages ? messages.map(message=> <h3 key={message.id}>{message.content}</h3>) : <h3>No hay mensajes</h3>
         } */}
-        <button onClick={logout}>Logout</button>
-        </>
-    )
+			<button onClick={logout}>Logout</button>
+		</>
+	)
 }
