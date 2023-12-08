9f78398ec3857a58fbf5f8a5e9b0b415f1d585c0
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-07 21:04:02 -0300

    Change name of parameter

--- App/src/context/AuthProvider.jsx
+++ App/src/context/AuthProvider.jsx
@@ -5,22 +5,22 @@ export const AuthContext = createContext()
 // const initialState = JSON.parse(window.localStorage.getItem('auth')) ? JSON.parse(window.localStorage.getItem('auth')) : {}
 
 export const AuthProvider = ({children}) =>{
-    const [auth,setAuth] = useState({})
+	const [auth,setAuth] = useState({})
 
-    const updateAuth = (result) => {
-        const newAuth = {...result}
-        // window.localStorage.setItem('auth', JSON.stringify(newAuth))
-        setAuth(newAuth)
-    }
+	const updateAuth = (result) => {
+		const newAuth = {...result}
+		// window.localStorage.setItem('auth', JSON.stringify(newAuth))
+		setAuth(newAuth)
+	}
 
-    const updateAuthLogin = (newAuth) => {
-        // window.localStorage.setItem('auth', JSON.stringify(newAuth))
-        setAuth(newAuth)
-    }
+	const updateAuthLogin = ({accessToken}) => {
+		// window.localStorage.setItem('auth', JSON.stringify(newAuth))
+		setAuth(accessToken)
+	}
 
-    return(
-        <AuthContext.Provider value={{auth,updateAuth,updateAuthLogin}}>
-            {children}
-        </AuthContext.Provider>
-    )
+	return(
+		<AuthContext.Provider value={{auth,updateAuth,updateAuthLogin}}>
+			{children}
+		</AuthContext.Provider>
+	)
 }
