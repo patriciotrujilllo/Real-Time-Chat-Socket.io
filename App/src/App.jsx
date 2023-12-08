dbdbf3510db68c1d87b93c22e2e3f826caa3b33d
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-07 21:03:19 -0300

    remove test components

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -7,49 +7,50 @@ import { Messages } from './components/Messages'
 import { Register } from './components/Register'
 import { RequireAuth } from './components/RequireAuth'
 import styled from 'styled-components'
-import { Prueba } from './components/prueba'
 import { Unauthorized } from './components/Unauthorized'
 import PersistLogin from './components/PersistLogin'
+// import Chat from './components/Chat'
 
 function App () {
 
-const socket = io('http://localhost:4000')
+	const socket = io('http://localhost:4000')
 
-useEffect(() => {
-  socket.on('connect', () => {
-    console.log('Conectado al servidor');
-  })
-// eslint-disable-next-line react-hooks/exhaustive-deps
-}, []);
+	useEffect(() => {
+		socket.on('connect', () => {
+			console.log('Conectado al servidor');
+		})
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-  return (
-    <Container>
-    <Suspense fallback={<div>Loading...</div>}>
-      <BrowserRouter>
-        <Routes>
-          {/* Rutas no protegidas/Publicas */}
-          <Route path="/" element={<Register/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/unauthorized" element={<Unauthorized/>}/>
+	return (
+		<Container>
+			<Suspense fallback={<div>Loading...</div>}>
+				<BrowserRouter>
+					<Routes>
+						{/* Rutas no protegidas/Publicas */}
+						<Route path="/" element={<Register/>}/>
+						<Route path="/login" element={<Login/>}/>
+						<Route path="/unauthorized" element={<Unauthorized/>}/>
 
-          <Route element={<PersistLogin/>}>
-            {/* Rutas protegidas, en vez de acerlo una a una, se envuelve todo a la vez*/}
-            {/* Rutas donde pueden ingresar usuarios y admimintradores */}
-            <Route element={<RequireAuth allowedRoles={[1,2]} />}>
-              <Route path="/messages" element={<Messages/>}/>
-            </Route>
+						<Route element={<PersistLogin/>}>
+							{/* Rutas protegidas, en vez de acerlo una a una, se envuelve todo a la vez*/}
+							{/* Rutas donde pueden ingresar usuarios y admimintradores */}
+							<Route element={<RequireAuth allowedRoles={[1,2]} />}>
+								<Route path="/messages" element={<Messages/>}/>
+								{/* <Route path="/chat" element={<Chat/>}/> */}
+							</Route>
 
-            {/* Rutas donde pueden ingresar admimintradores */}
-            <Route element={<RequireAuth allowedRoles={[1]} />}>
-              <Route path="/prueba" element={<Prueba/>}/>
-            </Route>
-          </Route>
+							{/* Rutas donde pueden ingresar admimintradores */}
+							<Route element={<RequireAuth allowedRoles={[1]} />}>
+								
+							</Route>
+						</Route>
 
-        </Routes>
-      </BrowserRouter>
-    </Suspense>
-    </Container>
-  )
+					</Routes>
+				</BrowserRouter>
+			</Suspense>
+		</Container>
+	)
 }
 
 const Container = styled.div`
