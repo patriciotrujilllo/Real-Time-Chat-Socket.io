492b22ad8f8cc67cc6e899930efa3249078c05b0
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-07 21:02:46 -0300

    update eslint

--- App/src/services/axios.js
+++ App/src/services/axios.js
@@ -3,18 +3,18 @@ import axios from 'axios'
 const PORT = 'http://localhost:4000'
 
 export const instancePrivate = axios.create({
-    baseURL: PORT,
-    headers: { 'Content-Type': 'application/json' },
-    withCredentials: true,
+	baseURL: PORT,
+	headers: { 'Content-Type': 'application/json' },
+	withCredentials: true,
 })
 
 export const instancePublic = axios.create({
-    baseURL: PORT,
-    headers: { 'Content-Type': 'application/json' }
+	baseURL: PORT,
+	headers: { 'Content-Type': 'application/json' }
 })
 
 export const instanceRefresh = axios.create({
-    baseURL: PORT,
-    headers: { 'Content-Type': 'application/json' },
-    withCredentials: true
+	baseURL: PORT,
+	headers: { 'Content-Type': 'application/json' },
+	withCredentials: true
 })
