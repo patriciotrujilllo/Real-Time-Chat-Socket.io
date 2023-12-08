c639c69d749251e671e33a8446b66df59cc0aea7
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-07 21:02:17 -0300

    update eslint

--- App/src/hooks/useAxiosRefresh.js
+++ App/src/hooks/useAxiosRefresh.js
@@ -4,44 +4,44 @@ import useRefreshToken from "./useRefreshToken";
 import { useAuth } from "./useAuth";
 
 const useAxiosRefresh = () => {
-    const refresh = useRefreshToken()
-    const {auth} = useAuth()
+	const refresh = useRefreshToken()
+	const {auth} = useAuth()
 
-    useEffect(()=> {
+	useEffect(()=> {
 
-        const requestIntercept = instancePrivate.interceptors.request.use(
-            config => {
-                if(!config.headers['Authorization']){
-                    config.headers['Authorization'] = `Bearer ${auth?.accessToken}`
-                }
-                return config
-            },
-            (error) => Promise.reject(`error en la request : ${error}`)
-        )
+		const requestIntercept = instancePrivate.interceptors.request.use(
+			config => {
+				if(!config.headers['Authorization']){
+					config.headers['Authorization'] = `Bearer ${auth?.accessToken}`
+				}
+				return config
+			},
+			(error) => Promise.reject(`error en la request : ${error}`)
+		)
         
-        const responseIntercept = instancePrivate.interceptors.response.use(
-            response=> response,
+		const responseIntercept = instancePrivate.interceptors.response.use(
+			response=> response,
 
-            async (error) => {
-                const prevRequest = error?.config
-                if(error?.response?.status === 403 && !prevRequest?.send){
-                    prevRequest.send = true
-                    const newAcccessToken = await refresh()
-                    prevRequest.headers['Authorization'] = `Bearer ${newAcccessToken}`
-                    return instancePrivate(prevRequest)
-                }
-                (error) => Promise.reject(`error en la response : ${error}`)
-            }
-        )
+			async (error) => {
+				const prevRequest = error?.config
+				if(error?.response?.status === 403 && !prevRequest?.send){
+					prevRequest.send = true
+					const newAcccessToken = await refresh()
+					prevRequest.headers['Authorization'] = `Bearer ${newAcccessToken}`
+					return instancePrivate(prevRequest)
+				}
+				(error) => Promise.reject(`error en la response : ${error}`)
+			}
+		)
 
-        return () => {
-            instancePrivate.interceptors.request.eject(requestIntercept)
-            instancePrivate.interceptors.response.eject(responseIntercept)
-        }
+		return () => {
+			instancePrivate.interceptors.request.eject(requestIntercept)
+			instancePrivate.interceptors.response.eject(responseIntercept)
+		}
 
-    },[auth,refresh])
+	},[auth,refresh])
 
-    return instancePrivate
+	return instancePrivate
 }
 
 export default useAxiosRefresh
