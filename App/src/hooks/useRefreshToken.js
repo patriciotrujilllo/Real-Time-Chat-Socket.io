73a8c8b066dec52f3f5e36bbded69cf3453ca852
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-07 21:02:30 -0300

    update eslint

--- App/src/hooks/useRefreshToken.js
+++ App/src/hooks/useRefreshToken.js
@@ -2,16 +2,16 @@ import { refreshToken } from "../services/auth";
 import { useAuth } from "./useAuth";
 
 const useRefreshToken = () => {
-    const {updateAuth} = useAuth()
+	const {updateAuth} = useAuth()
 
-    const refresh = async() => {
+	const refresh = async() => {
 
-        const result = await refreshToken()
-        // const data = {accessToken: result.accessToken, email: result.email , roleId : result.roleId}
-        updateAuth(result)
-        return result
-    }
-    return refresh
+		const result = await refreshToken()
+		// const data = {accessToken: result.accessToken, email: result.email , roleId : result.roleId}
+		updateAuth(result)
+		return result
+	}
+	return refresh
 }
 
 export default useRefreshToken
