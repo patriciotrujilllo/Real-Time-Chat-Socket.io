c47e384ffb9adf940b9301dd14376b88063124f7
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-07 21:13:36 -0300

    auth is in accesToken, import jwt-decode

--- App/src/components/RequireAuth.jsx
+++ App/src/components/RequireAuth.jsx
@@ -1,14 +1,22 @@
 import { Navigate,Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import { jwtDecode } from 'jwt-decode'
 
 export const RequireAuth = ({ allowedRoles }) =>{
-    const {auth} = useAuth()
-    const location = useLocation()
-    return (
-        allowedRoles?.includes(auth?.roleId) ?
-        <Outlet /> 
-        : auth?.email ? 
-        <Navigate to="/unauthorized" state={{ from: location }} replace />
-        :<Navigate to="/login" state={{ from: location }} replace />
-    )
+	const {auth} = useAuth()
+	const location = useLocation()
+
+	const decoded = auth?.accessToken ?
+		jwtDecode(auth?.accessToken)
+		: undefined
+
+	const roleId = decoded?.roleId || []
+
+	return (
+		allowedRoles?.includes(roleId) ?
+			<Outlet /> 
+			: auth?.accessToken ? 
+				<Navigate to="/unauthorized" state={{ from: location }} replace />
+				:<Navigate to="/login" state={{ from: location }} replace />
+	)
 }
