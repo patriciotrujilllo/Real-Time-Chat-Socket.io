f1fedd52890e1b9af77e75cb9f19db8572919abf
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-11 22:52:02 -0300

    inprove

--- Server/src/utils/pathOfImg.js
+++ Server/src/utils/pathOfImg.js
@@ -4,13 +4,14 @@ export const ulrImage = (file) =>{
     const arrayPath = file.path.split('\\')
     const nameImg = arrayPath.pop()
     const folderImg = arrayPath.pop()
-    const pathToSave = `${folderImg}/${nameImg}`
+    // const pathToSave = `${folderImg}/${nameImg}`
+    const pathToSave = `${nameImg}`
     return pathToSave
 }
 export const deletelinkFile = ({path}) =>{
     try {
         if(!path) throw new Error('there is not image to delete')
-        fs.unlinkSync(`src/uploads/${path}`)
+        fs.unlinkSync(`src/uploads/users/${path}`)
     } catch (error) {
         console.error(error)
     }
