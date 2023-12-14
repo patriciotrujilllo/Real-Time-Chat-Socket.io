b07bcb7005ec81cc5ea575b86e2e684a54ba297e
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-12-14 18:57:39 -0300

    syled component, button of logout, three parts, header, messages, and input, request to all messages, and function to send a message to backend

--- App/src/components/ChatContainer.jsx
+++ App/src/components/ChatContainer.jsx
@@ -1,8 +1,90 @@
-const ChatContainer = ({currentChat}) => {
+import styled from "styled-components"
+import Logout from "./Logout"
+import ChatInput from "./ChatInput"
+import Messages from "./Messages"
+import { useEffect, useState } from "react"
+import useAxiosRefresh from "../hooks/useAxiosRefresh"
+
+
+const ChatContainer = ({currentChat,currentUser}) => {
+
+	const [messages,setMessages] = useState([])
+	const axios = useAxiosRefresh()
+
+
+	useEffect(()=> {
+		(async()=>{
+			const response = await axios.post('/message/user',{idReceptor: currentChat.id})
+			setMessages(response?.data)
+		}
+		)()
+
+	},[currentChat])
+
+	const sendMessage = async(msg) => {
+		try {
+			const messagetoSend = {
+				idEmitor: currentUser.id,
+				idReceptor: currentChat.id,
+				content: msg
+			}
+			const result = await axios.post('/message',messagetoSend)
+			
+			setMessages(prevState => [...prevState,result.data])
+		} catch (err) {
+			console.error(err)
+		}
+	}
 
 	return (
-		<h2>Holaaaaa{currentChat.id}</h2>
+		<Container>
+			<div className="chat-header">
+				<div className="user-details">
+					<div className="avatar">
+						<img
+							src={currentChat.img}
+							alt=""
+						/>
+					</div>
+					<div className="username">
+						<h3>{currentChat.firstName}</h3>
+					</div>
+				</div>
+				<Logout/>
+			</div>
+			<Messages messages={messages} currentUser={currentUser}/>
+			<ChatInput sendMessage = {sendMessage}/>
+		</Container>
 	)
 }
+
+const Container = styled.div`
+	display: grid;
+	grid-template-rows: 1fr 8fr 1fr;
+	.chat-header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0 2rem;
+		background-color: #080420;
+		.user-details{
+			display:flex;
+			align-items: center;
+			gap: 1rem;
+			.avatar {
+				img {
+					height: 3rem;
+					width: 3rem;
+                border-radius: 0.4rem;
+				}
+			}
+			.username {
+				h3 {
+					color: white;
+				}
+			}
+		}
+	}
+`
 
 export default ChatContainer
